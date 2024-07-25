5df8a1e032ef69048159caf871d9ab3ee342594a
Toninnn <126908275+Toninnn@users.noreply.github.com>
2024-07-25 15:49:17 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_Asp.net_portifolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
