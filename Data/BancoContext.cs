a97179b9556b1e0ff11cf253fac0180d8d8309a0
Toninnn <126908275+Toninnn@users.noreply.github.com>
2024-07-28 21:58:49 -0300

    Começando BancodeDados

---
+++ Data/BancoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Asp.net_portifolio.Models;
+
+namespace MVC_Asp.net_portifolio.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ContatoModel> Contatos {  get; set; }
+    }
+}
---
+++ Models/ContatoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_Asp.net_portifolio.Models
+{
+    public class ContatoModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; } 
+    }
+}
